dd98948c32d08ab55f7b46974f2fdd88e4ea74dc
Cauã Sebastian <cauasebastian2207@gmail.com>
2024-03-17 16:24:34 -0300

    media for mobile

--- html/script.js
+++ html/script.js
@@ -1,14 +1,15 @@
+// JavaScript code
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 const close = document.getElementById('close');
 
 if(bar){
-    bar.addEventListener('click', () =>{
-    nav.classList.add('active');
-    })
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    });
 }
 if(close){
-    close.addEventListener('click', () =>{
-    nav.classList.remove('active');
-    })
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    });
 }
